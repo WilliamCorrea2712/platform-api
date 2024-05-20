3d6d21f9c90d7e7f898b75dbb17c2297b876dd3b
William Correa <william.correa.dev@gmail.com>
2024-05-20 20:25:38 -0300

    updates cart

--- models/checkout/cart.php
+++ models/checkout/cart.php
@@ -7,23 +7,49 @@ class CartModel {
     public function getProductsCart($user_id, $session_id, $customer_id) {
         global $conn;
     
-        $sql = "SELECT * FROM " . PREFIX . "temporary_cart WHERE ";
+        $sql = "SELECT 
+                cart.*, 
+                product.price, 
+                product.weight, 
+                product.length, 
+                product.width, 
+                product.height,
+                description.name,
+                attribute.value,
+                parent_attribute.value AS parent_attribute_value,
+                image.url AS image_url
+            FROM " . PREFIX . "temporary_cart AS cart
+            INNER JOIN " . PREFIX . "product AS product ON cart.product_id = product.product_id
+            INNER JOIN " . PREFIX . "product_description AS description ON product.product_id = description.product_id
+            INNER JOIN " . PREFIX . "product_attribute_value AS attribute ON cart.id = attribute.id
+            LEFT JOIN " . PREFIX . "product_attribute_value AS parent_attribute ON attribute.parent_attribute_id = parent_attribute.id
+            LEFT JOIN (
+                SELECT product_id, url
+                FROM " . PREFIX . "product_image
+                WHERE sort_order = (
+                    SELECT MIN(sort_order)
+                    FROM " . PREFIX . "product_image AS img
+                    WHERE img.product_id = " . PREFIX . "product_image.product_id
+                )
+                GROUP BY product_id
+            ) AS image ON product.product_id = image.product_id
+            WHERE ";
     
         $params = [];
         $types = ""; 
     
         if (!empty($customer_id) && !empty($session_id)) {
-            $sql .= "customer_id = ? AND session_id = ?";
+            $sql .= "cart.customer_id = ? AND cart.session_id = ?";
             $params[] = $customer_id;
             $types .= "i";
             $params[] = $session_id;
             $types .= "s";
         } elseif (!empty($customer_id)) {
-            $sql .= "customer_id = ?";
+            $sql .= "cart.customer_id = ?";
             $params[] = $customer_id;
             $types .= "i";
         } elseif (!empty($session_id)) {
-            $sql .= "session_id = ?";
+            $sql .= "cart.session_id = ?";
             $params[] = $session_id;
             $types .= "s";
         } else {
@@ -42,6 +68,6 @@ class CartModel {
         }
     
         return $products;
-    }    
+    }         
 }
 ?>
